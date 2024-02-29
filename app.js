82ce7232de61cd37e3e001fd2ed7430d7beb110b
Intrum1 <skoropadskiy1979@gmail.com>
2024-02-29 14:29:14 +0100

    Clear_rep_mongo

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
-import contactsRouter from "./routes/contactsRouter.js";
 import userRouter from "./routes/userRoutes.js";
 
 const app = express();
@@ -15,7 +14,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/api/users", userRouter);
-app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
@@ -25,7 +23,5 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
-
-
 
 export default app;
