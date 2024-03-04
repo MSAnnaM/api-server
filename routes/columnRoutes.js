728717b293caf4eaf49f20d5729d44418b14861e
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-03-04 21:10:08 +0100

    column
    columnController. columnRouter. ColumnService

--- routes/columnRoutes.js
+++ routes/columnRoutes.js
@@ -2,7 +2,37 @@ import { express } from "express";
 import { verifyToken } from "../midellwares/checkToken";
 import checkIsValidId from "../midellwares/isValidId";
 import { getAllColumnByBoard } from "../services/columnService";
+import validateBody from "../helpers/validateBody";
+import {
+  createColumnSchema,
+  updateColumnSchema,
+} from "../schemas/columnSchema";
+import {
+  createColumn,
+  removeColumn,
+  updateColumn,
+} from "../controllers/columnControllers";
 
 columnRouter = express.Router();
 
 columnRouter.get("/:bordId", verifyToken, checkIsValidId, getAllColumnByBoard);
+
+columnRouter.post(
+  "/:boardId",
+  verifyToken,
+  checkIsValidId,
+  validateBody(createColumnSchema),
+  createColumn
+);
+
+columnRouter.put(
+  "/:columnId",
+  verifyToken,
+  checkIsValidId,
+  validateBody(updateColumnSchema),
+  updateColumn
+);
+
+columnRouter.delete("/:columnId", verifyToken, checkIsValidId, removeColumn);
+
+export default columnRouter;
