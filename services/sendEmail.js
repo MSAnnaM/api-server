6efd3d30f32ad8890020c285eb474203b38616b4
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2024-03-24 19:36:47 +0200

    update

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -15,7 +15,7 @@ export const sendMail = async (email, comment) => {
 
   const mailOptions = {
     from: UKR_NET_FROM,
-    to: "taskpro.project@gmail.com",
+    to: "momotanna.m@gmail.com",
     subject: "Need Help",
     html: `<h1>Email to answer: ${email}<br>Comment: ${comment}</h1>`,
     text: `Email to answer: ${email}\nComment: ${comment}`,
